3e42cd14a2b691cdde22aa0311f226788e28b8ba
Pallvi <raopallvi599@gmail.com>
2023-06-23 14:58:04 +0530

    Project Completed

--- assets/js/index.js
+++
@@ -1,6 +0,0 @@
-function handleCheckboxClick(event) {
-    event.stopPropagation(); // Stop the click event from propagating further
-
-    const anchor = event.target.parentNode; // Get the parent anchor element
-    anchor.click(); // Trigger the anchor's click event
-  }
